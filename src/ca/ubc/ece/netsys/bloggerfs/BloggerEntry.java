51d008fbde3935845f32cc6153f0e2c56163e272
elsantosneto <elsantosneto@41f98e45-9624-0410-815f-53db5dbcbaab>
2007-09-20 05:03:00 +0000

    New source code documentation and scripts updated.

--- src/ca/ubc/ece/netsys/bloggerfs/BloggerEntry.java
+++ src/ca/ubc/ece/netsys/bloggerfs/BloggerEntry.java
@@ -1,3 +1,23 @@
+/*
+ * Copyright (c) 2007 Networked Systems Lab - http://www.ece.ubc.ca
+ * 
+ * Electrical and Computer Engineering Department - University of British Columbia
+ *  
+ * 
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
+ */
 package ca.ubc.ece.netsys.bloggerfs;
 
 import com.google.gdata.data.Entry;
